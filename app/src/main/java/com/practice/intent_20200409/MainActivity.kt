3232ea88161d7cbff39dc495279598bdaa5f51cb
jykim7760 <jykim7760@naver.com>
2020-04-09 20:52:43 +0900

    직접입력하는 로직

--- app/src/main/java/com/practice/intent_20200409/MainActivity.kt
+++ app/src/main/java/com/practice/intent_20200409/MainActivity.kt
@@ -19,6 +19,16 @@ class MainActivity : AppCompatActivity() {
 
 fun setupEvents(){
     dialBTN.setOnClickListener {
+
+//        입력한 번호로 전화를 걸도록 직접 코드 수정
+//
+//        val myUri = Uri.parse(Intent.ACTION_DIAL, callBtn)
+//        val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+//
+val inputPhoneNum = phoneNumEdt.text.toString()
+
+
+
 //        어느 화면으로 갈지 명시 -> uri를 이용해서 명시
         val myUri = Uri.parse("tel:01099237760")
         val myIntent = Intent(Intent.ACTION_DIAL, myUri)
